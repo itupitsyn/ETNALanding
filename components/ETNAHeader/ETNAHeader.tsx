f7d3ce05854c88e753c6f09231da558301b3cca8
itupitsyn <itupitsyn@ptsecurity.com>
2026-01-14 00:46:15 +0300

    header

--- components/ETNAHeader/ETNAHeader.tsx
+++ components/ETNAHeader/ETNAHeader.tsx
@@ -5,8 +5,8 @@ import { Button } from '../ui/Button';
 
 export const ETNAHeader: FC = () => {
   return (
-    <div className="xs:pb-4.5 flex h-screen max-h-231.25 w-screen flex-col justify-end bg-[url(/logo-bg-optimized.png)] px-5 pb-14">
-      <div className="xs:flex-row xs:items-end xs:justify-start xs:flex-wrap xs:gap-y-4 flex flex-col items-start">
+    <div className="xs:pb-4.5 flex h-screen max-h-231.25 w-full flex-col justify-end pb-14 before:absolute before:top-0 before:left-0 before:z-[-1] before:h-full before:max-h-231.25 before:w-full before:rotate-180 before:bg-[url(/logo-bg-optimized.png)] before:bg-top before:bg-no-repeat before:content-['']">
+      <div className="xs:flex-row xs:items-end xs:justify-start xs:flex-wrap xs:gap-y-4 container flex flex-col items-start">
         <ETNALogo className="text-lighter xs:w-76.5 xs:h-22 max-w-full flex-none lg:h-28.5 lg:w-98.5" />
 
         <p className="text-lighter xs:mt-0 xs:ml-4.25 mt-4 text-2xl leading-none font-light tracking-tighter whitespace-pre-wrap lg:text-[28px]">
--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -5,27 +5,29 @@ import { BurgerMenu } from './components/BurgerMenu';
 
 export const Navigation: FC = () => {
   return (
-    <nav className="text-lighter xs:top-3 absolute top-5 left-0 flex w-screen justify-between gap-2 px-5 py-3 font-light tracking-tighter lg:top-6 lg:text-xl">
-      <div className="xs:gap-10.5 flex gap-8 lg:gap-27">
-        <p>
-          <span className="font-bold">ETNA</span>
-          <span>SOFTWARE</span>
-        </p>
-        <ul className="xs:flex hidden gap-8 whitespace-nowrap lg:gap-10.75">
-          <li>
-            <Link href="#">О нас</Link>
-          </li>
-          <li>
-            <Link href="#">Проекты</Link>
-          </li>
-        </ul>
-      </div>
+    <div className="absolute top-5 left-0 w-screen">
+      <nav className="text-lighter xs:top-3 container flex w-full justify-between gap-2 py-3 font-light tracking-tighter lg:top-6 lg:text-xl">
+        <div className="xs:gap-10.5 flex gap-8 lg:gap-27">
+          <p>
+            <span className="font-bold">ETNA</span>
+            <span>SOFTWARE</span>
+          </p>
+          <ul className="xs:flex hidden gap-8 whitespace-nowrap lg:gap-10.75">
+            <li>
+              <Link href="#">О нас</Link>
+            </li>
+            <li>
+              <Link href="#">Проекты</Link>
+            </li>
+          </ul>
+        </div>
 
-      <Link href="mailto:corp@etna.software" className="xs:block hidden transition-colors hover:text-white">
-        corp@etna.software
-      </Link>
+        <Link href="mailto:corp@etna.software" className="xs:block hidden transition-colors hover:text-white">
+          corp@etna.software
+        </Link>
 
-      <BurgerMenu className="xs:hidden lg:text-2xl" />
-    </nav>
+        <BurgerMenu className="xs:hidden lg:text-2xl" />
+      </nav>
+    </div>
   );
 };
